0182a7a6b89e2bf519e3da553415052d15df85f8
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-07-29 18:07:58 +0400

    - Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 29.07.22.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
