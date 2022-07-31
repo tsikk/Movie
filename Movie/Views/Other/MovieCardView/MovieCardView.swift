db93f01c496394a20c586113138a3fb30ba64b4c
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-07-31 15:50:40 +0400

    Added MovieCardView
    It is a helper view for List

---
+++ Movie/Views/Other/MovieCardView/MovieCardView.swift
@@ -0,0 +1,22 @@
+//
+//  MovieCardView.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 31.07.22.
+//
+
+import SwiftUI
+
+struct MovieCardView: View {
+    var movie: Movie
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+//struct MovieCardView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MovieCardView(movie: )
+//    }
+//}
