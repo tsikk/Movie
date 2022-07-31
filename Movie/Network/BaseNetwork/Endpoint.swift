e616af3c3b5f93f460f0601f5e6235c291879519
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-07-31 15:20:23 +0400

    Added network in the project
    - Endpoint is a protocol in which all endpoints need to conform to inform all their details: scheme, host, path, method, header and body.
    - The Endpoint protocol has a default implementation of scheme and host as it is normally just one for all endpoints. Therefore, it is not necessary to implement it all the time. If necessary, you can override this implementation.
    - The type of the method variable is RequestMethod and represents the HTTP methods of the endpoint, which can be: GET, POST, PUT, DELETE, PATCH, HEAD, etc. For this API, only the top five methods are included, but you can add more if it makes sense in your context.

---
+++ Movie/Network/BaseNetwork/Endpoint.swift
@@ -0,0 +1,27 @@
+//
+//  Endpoint.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 31.07.22.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var scheme: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String]? { get }
+    var body: [String: String]? { get }
+}
+
+extension Endpoint {
+    var scheme: String {
+        return "https"
+    }
+
+    var host: String {
+        return "api.themoviedb.org"
+    }
+}
