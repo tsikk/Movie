5222f7c432fd3a644f3cd5ac3a62b88cfe5bcc30
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-07-31 15:24:06 +0400

    Added models for network layer
    - The TopRated struct is a Decodable model to decode the response from the top_rated endpoint of the TMDB API.
     Note: it has the CodingKeys enum which was used to rename the Snake case entries (sent by the API, as total_pages) to Camel case.

---
+++ Movie/Network/Models/TopRated.swift
@@ -0,0 +1,28 @@
+//
+//  TopRated.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 31.07.22.
+//
+
+import Foundation
+
+struct TopRated: Decodable, Hashable {
+    let page: Int
+    let totalPages: Int
+    let totalResults: Int
+    let results: [Movie]
+
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+extension TopRated: Identifiable {
+    var id: Int {
+        page
+    }
+}
