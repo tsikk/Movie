6f5e2325ee3de89091806f5920e99f0e2ebd519f
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-07-31 15:26:28 +0400

    Added MoviesEndpoint file
    - For all endpoints within the Movies service, we will use MoviesEndpoint.
    - MoviesEndpoint is an enum that conforms to the Endpoint protocol. It has all the information needed for each endpoint. To add a new Movies service endpoint, just add a new case and complete the information inside each variable: path, method, header and body.

---
+++ Movie/Network/Endpoints/MoviesEndpoint.swift
@@ -0,0 +1,49 @@
+//
+//  MoviesEndpoint.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 31.07.22.
+//
+
+import Foundation
+
+enum MoviesEndpoint {
+    case topRated
+    case movieDetail(id: Int)
+}
+
+extension MoviesEndpoint: Endpoint {
+    var path: String {
+        switch self {
+        case .topRated:
+            return "/3/movie/top_rated"
+        case .movieDetail(let id):
+            return "/3/movie/\(id)"
+        }
+    }
+
+    var method: RequestMethod {
+        switch self {
+        case .topRated, .movieDetail:
+            return .get
+        }
+    }
+
+    var header: [String: String]? {
+        let accessToken = "5ce75b33377ea321b6fad2bee7baba1e"
+        switch self {
+        case .topRated, .movieDetail:
+            return [
+                "Content-Type": "application/json;charset=utf-8",
+                "Authorization": "\(accessToken)"
+            ]
+        }
+    }
+    
+    var body: [String: String]? {
+        switch self {
+        case .topRated, .movieDetail:
+            return nil
+        }
+    }
+}
