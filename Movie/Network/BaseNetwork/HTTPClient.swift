e79d875b4d8b778cc90588bb8085e5b6243f32f2
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-07-31 15:25:22 +0400

    Created generic network layer with async/await
    - HTTPClient is a protocol with a default implementation of the sendRequest function, responsible for making the request to the server. As a parameter, it receives the Endpoint and the type of the model to decode the API response.
    - As a return, we’ll have a Result that can be a success with the decoded response or an error with one of the cases created in RequestError.

---
+++ Movie/Network/BaseNetwork/HTTPClient.swift
@@ -0,0 +1,57 @@
+//
+//  HTTPClient.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 31.07.22.
+//
+
+import Foundation
+
+protocol HTTPClient {
+    func sendRequest<T: Decodable>(endpoint: Endpoint, responseModel: T.Type) async -> Result<T, RequestError>
+}
+
+extension HTTPClient {
+    func sendRequest<T: Decodable>(
+        endpoint: Endpoint,
+        responseModel: T.Type
+    ) async -> Result<T, RequestError> {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = endpoint.scheme
+        urlComponents.host = endpoint.host
+        urlComponents.path = endpoint.path
+
+        let urli = URL(string: "https://api.themoviedb.org/3/movie/top_rated?api_key=5ce75b33377ea321b6fad2bee7baba1e")
+        guard let url = urli else {
+            return .failure(.invalidURL)
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = endpoint.method.rawValue
+        request.allHTTPHeaderFields = endpoint.header
+        
+        if let body = endpoint.body {
+            request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: [])
+        }
+        
+        do {
+            let (data, response) = try await URLSession.shared.data(for: request, delegate: nil)
+            guard let response = response as? HTTPURLResponse else {
+                return .failure(.noResponse)
+            }
+            switch response.statusCode {
+            case 200...299:
+                guard let decodedResponse = try? JSONDecoder().decode(responseModel, from: data) else {
+                    return .failure(.decode)
+                }
+                return .success(decodedResponse)
+            case 401:
+                return .failure(.unauthorized)
+            default:
+                return .failure(.unexpectedStatusCode)
+            }
+        } catch {
+            return .failure(.unknown)
+        }
+    }
+}
