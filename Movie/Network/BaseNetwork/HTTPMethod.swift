2088f4b15f1d1db742d796eaed3c9200bf38eaad
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-07-31 15:22:44 +0400

    Added Request error and methods for network
    - The type of the method variable is RequestMethod and represents the HTTP methods of the endpoint, which can be: GET, POST, PUT, DELETE, PATCH, HEAD, etc.
    - The RequestError enum contains some possible errors that the further HTTPClient will identify and return so that it can be handled by the app.
    - RequestError enum  has the customMessage variable, which can be used to display a feedback message to the user or to track errors.

---
+++ Movie/Network/BaseNetwork/HTTPMethod.swift
@@ -0,0 +1,16 @@
+//
+//  HTTPMethod.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 31.07.22.
+//
+
+import Foundation
+
+enum RequestMethod: String {
+    case delete = "DELETE"
+    case get = "GET"
+    case patch = "PATCH"
+    case post = "POST"
+    case put = "PUT"
+}
---
+++ Movie/Network/BaseNetwork/RequestError.swift
@@ -0,0 +1,28 @@
+//
+//  RequestError.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 31.07.22.
+//
+
+import Foundation
+
+enum RequestError: Error {
+    case decode
+    case invalidURL
+    case noResponse
+    case unauthorized
+    case unexpectedStatusCode
+    case unknown
+    
+    var customMessage: String {
+        switch self {
+        case .decode:
+            return "Decode error"
+        case .unauthorized:
+            return "Session expired"
+        default:
+            return "Unknown error"
+        }
+    }
+}
