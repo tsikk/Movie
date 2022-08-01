dde013e56467d6dd53eb279d0423dda45d4a73fd
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-08-01 09:45:37 +0400

    Added RatingView
    RatingView reflects actual rating with stars

---
+++ Movie/Views/Other/RatingView.swift
@@ -0,0 +1,49 @@
+//
+//  RatingVirw.swift
+//  Movie
+//
+//  Created by Ilia Tsikelashvili on 31.07.22.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    
+    var rating: Int
+
+    var maximumRating = 5
+
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+
+    var offColor = Color.gray
+    var onColor = Color.yellow
+    
+    var body: some View {
+        HStack {
+
+            ForEach(1..<maximumRating + 1, id: \.self) { number in
+                image(for: number)
+                    .foregroundColor(number > rating ? offColor : onColor)
+            }
+            
+            Text("\(rating)/10")
+                .bold()
+                .foregroundColor(.white)
+        }
+    }
+    
+    func image(for number: Int) -> Image {
+        if number > rating {
+            return offImage ?? onImage
+        } else {
+            return onImage
+        }
+    }
+}
+
+struct RatingView_Previews: PreviewProvider {
+    static var previews: some View {
+        RatingView(rating: 3)
+    }
+}
