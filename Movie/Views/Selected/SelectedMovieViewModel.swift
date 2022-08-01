deef7babd76bc93a4a48c94e92832890d6df9618
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-08-01 09:49:55 +0400

    Updated SelectedMovieViewModel
    We are modifying fetched model in the SelectedMovieViewModel

--- Movie/Views/Selected/SelectedMovieViewModel.swift
+++ Movie/Views/Selected/SelectedMovieViewModel.swift
@@ -9,12 +9,29 @@ import Foundation
 
 class SelectedMovieViewModel: BaseViewModel {
     var movie: Movie
-    
+    let firstPath = "https://image.tmdb.org/t/p/w400"
+
     init(with movie: Movie) {
         self.movie = movie
     }
     
     required init() {
         fatalError("init() has not been implemented")
+    }
+    
+    var releaseDate: String {
+        movie.convertedReleaseDate() ?? "unknown"
+    }
+    
+    var rating: Int {
+        Int(movie.voteAverage / 2)
+    }
+    
+    var posterURL: URL? {
+       URL(string: firstPath + movie.backdropPath) 
+    }
+    
+    var overview: String {
+        movie.overview
     }
 }
