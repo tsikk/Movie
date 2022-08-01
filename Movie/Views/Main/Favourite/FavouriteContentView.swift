4509f0b49ba70e291c1f3efaf3500deb8db18081
Ilia Tsikelashvili <84790229+tsikk@users.noreply.github.com>
2022-08-01 17:26:57 +0400

    Modified FavouriteContentView

--- Movie/Views/Main/Favourite/FavouriteContentView.swift
+++ Movie/Views/Main/Favourite/FavouriteContentView.swift
@@ -9,54 +9,16 @@ import SwiftUI
 import Kingfisher
 
 struct FavouriteContentView: View {
-    
     @ObservedObject var viewModel: FavouriteViewModel
-    let firstPath = "https://image.tmdb.org/t/p/w400"
-    @State var isFavourite = false
 
     var body: some View {
-        List(viewModel.getAllMovies(), id: \.self) { movie in
-            ZStack(alignment: .bottom) {
-                KFImage(URL(string:firstPath + (movie.posterPath ?? "nil")))
-                    .placeholder {
-                        ProgressView()
-                    }
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(minWidth: 0, maxHeight: 400)
-                HStack {
-                    Text(movie.title ?? "unknown")
-                        .bold()
-                    Spacer()
-                    
-                    Button {
-                        viewModel.tappedOnFavourite(movie: movie)
-                    } label: {
-                        Image(systemName: isFavourite ? "star.fill" : "star")
-                            .foregroundColor(.red)
-                    }.buttonStyle(.bordered)
-                    
+        List(viewModel.movies, id: \.self) { movie in
+          MovieCardView(movie: movie, viewModel: viewModel)
+                .listRowInsets(EdgeInsets())
+                .listRowSeparator(.hidden)
+                .onTapGesture {
+                    viewModel.openSelectedMovie(with: movie)
                 }
-                .padding()
-                .background(.thinMaterial)
-            }
-            .background(.thickMaterial)
-            .mask(RoundedRectangle(cornerRadius: 16))
-            .padding(8)
-            .listRowInsets(EdgeInsets())
-            .listRowSeparator(.hidden)
-            .onTapGesture {
-                viewModel.openSelectedMovie(with: MoviesModel(movie: Movie(backdropPath: movie.backdropPath ?? "",
-                                                                           id: Int(movie.id),
-                                                                           overview: movie.overview ?? "",
-                                                                           popularity: movie.popularity,
-                                                                           posterPath: movie.posterPath ?? "",
-                                                                           releaseDate: movie.releaseDate ?? "",
-                                                                           title: movie.title ?? "",
-                                                                           voteAverage: movie.voteAverage,
-                                                                           voteCount: Int(movie.voteCount)),
-                                                              id: Int(movie.id)))
-            }
         }
         .listStyle(.plain)
         
