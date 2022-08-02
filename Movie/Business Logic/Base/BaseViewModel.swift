1cf2d757eb0e9130c3ef091ea2243d32d355d2c6
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-02 10:34:40 +0400

    deleted unused codes

--- Movie/Business Logic/Base/BaseViewModel.swift
+++ Movie/Business Logic/Base/BaseViewModel.swift
@@ -9,16 +9,6 @@ import Foundation
 
 class BaseViewModel: ViewModeling, ObservableObject {
 
-    private var counter = 0
-    
-    @Published var isLoading = false {
-        willSet {
-            objectWillChange.send()
-        }
-    }
-
-    var didChangeLoadingTo: ((Bool) -> Void)?
-
     required init() {
         initialSetup()
     }
@@ -28,38 +18,6 @@ class BaseViewModel: ViewModeling, ObservableObject {
     func viewDidAppear(_ animated: Bool) {}
     func viewWillDisappear(_ animated: Bool) {}
 
-    func retainLoading() {
-        if counter == 0 {
-            notifyLoadingUpdate(to: true)
-        }
-
-        counter += 1
-    }
-    func releaseLoading() {
-        guard counter > 0 else { return }
-
-        counter -= 1
-
-        if counter == 0 {
-            notifyLoadingUpdate(to: false)
-        }
-    }
-    func disableLoading() {
-        counter = 0
-        notifyLoadingUpdate(to: false)
-    }
-
-    // MARK: - Private
-
-    private func notifyLoadingUpdate(to isLoading: Bool) {
-        
-        DispatchQueue.main.async { [weak self] in
-            guard let self = self else { return }
-
-            self.isLoading = isLoading
-            self.didChangeLoadingTo?(isLoading)
-        }
-    }
     
     func tappedOnFavourite(movie: MoviesModel) {
         // needs to be overriden
--- Movie/Views/Main/Selected/SelectedMovieContentView.swift
+++ Movie/Views/Main/Selected/SelectedMovieContentView.swift
@@ -53,17 +53,3 @@ struct SelectedMovieContentView: View {
         .padding(.top, 16)
     }
 }
-
-//struct SelectedMovieContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        SelectedMovieContentView(viewModel: SelectedMovieViewModel(with: .init(backdropPath: "/kXfqcdQKsToO0OUXHcrrNCHDBzO.jpg",
-//                  id: 278,
-//                  overview: "Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
-//                  popularity: 81.971,
-//                  posterPath: "/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
-//                  releaseDate: "1994-09-23",
-//                  title: "The Shawshank Redemption",
-//                  voteAverage: 8.7,
-//                  voteCount: 21896)))
-//    }
-//}
