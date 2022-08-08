450595887c56736b713f7a45576d256e46db44af
Ilia Tsikelashvili <tsikelashviliilia@gmail.com>
2022-08-08 21:48:16 +0400

    Deleted redundant code for BaseRouting

--- Movie/Business Logic/Base/BaseRouting.swift
+++ Movie/Business Logic/Base/BaseRouting.swift
@@ -8,14 +8,9 @@
 import UIKit
 
 protocol BaseRouting {
-    var topViewController: UIViewController? { get }
     var anyTopController: UIViewController? { get }
-    var topSplitController: UISplitViewController? { get }
     var topNavigationController: UINavigationController? { get }
     var topTabBarController: UITabBarController? { get }
-    var tabTopNavigationController: UINavigationController? { get }
-    func show(_ viewController: UIViewController, on tab: Tabs)
-    func present(_ viewControllerToPresent: UIViewController, on tab: Tabs, animated: Bool, completion: (() -> Void)?)
 }
 
 extension BaseRouting {
@@ -26,13 +21,9 @@ extension BaseRouting {
     }
 
     var topViewController: UIViewController? {
-        let split = topSplitController
         let navigation = topNavigationController
-        let tabBar = topTabBarController
 
-        if split != nil || navigation != nil || tabBar != nil {
-            return nil
-        }
+        if navigation != nil { return nil }
 
         return rootController
     }
@@ -41,34 +32,11 @@ extension BaseRouting {
         rootController
     }
 
-    var topSplitController: UISplitViewController? {
-        rootController as? UISplitViewController
-    }
-
     var topNavigationController: UINavigationController? {
         rootController as? UINavigationController
     }
 
     var topTabBarController: UITabBarController? {
         rootController as? UITabBarController
-    }
-
-    var tabTopNavigationController: UINavigationController? {
-        topTabBarController?.selectedViewController as? UINavigationController
-    }
-
-    func show(_ viewController: UIViewController, on tab: Tabs) {
-        let rootController = topTabBarController?.viewControllers?[tab.rawValue]
-
-        rootController?.show(viewController, sender: nil)
-        topTabBarController?.selectedIndex = tab.rawValue
-    }
-
-    func present(_ viewControllerToPresent: UIViewController, on tab: Tabs, animated: Bool, completion: (() -> Void)?) {
-        let rootController = topTabBarController?.viewControllers?[tab.rawValue] as? UINavigationController
-        let presentationController = rootController?.viewControllers.last
-
-        presentationController?.present(viewControllerToPresent, animated: animated, completion: completion)
-        topTabBarController?.selectedIndex = tab.rawValue
     }
 }
